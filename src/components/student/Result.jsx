4e7e44a34354c4527be262bbdaf89c27a9b2025d
Ab5114 <sharmanitin5114@gmail.com>
2025-06-22 00:48:18 +0530

    add result

--- src/components/student/Result.jsx
+++ src/components/student/Result.jsx
@@ -1,17 +1,31 @@
- import { useLocation } from 'react-router-dom';
+ import { useLocation, useNavigate } from 'react-router-dom';
+import styles from "./Result.module.css";
 
 const Result = () => {
   const { state } = useLocation();
   const { exam, answers } = state;
+  const {subject,description}=exam;
+     const navigate = useNavigate();
 
   const correct = exam.questions.reduce((acc, q, i) => (
     acc + (answers[i] === q.answer ? 1 : 0)
   ), 0);
 
+  const handleBack=()=>{
+    navigate("/student");
+  }
+
   return (
     <div>
+          <div className={styles.header}>
+          <h2>{subject} - {description}</h2>
+       </div>
       <h2>Result</h2>
-      <p>Score: {correct} / {exam.questions.length}</p>
+     
+      <p className={styles.score}>Score: {correct*4} / {exam.questions.length*4} </p>
+      <div className={styles.controls}>
+      <button className={styles.nextBtn} onClick={handleBack}> Back</button>
+      </div>
     </div>
   );
 };
