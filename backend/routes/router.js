3e8901c2c0274d9c96b2575d40c6e33b43ce1d71
Ab5114 <sharmanitin5114@gmail.com>
2025-06-27 23:59:51 +0530

    add router

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const router = express.Router();
-
+const Exam = require("../models/Exam");
 const Exam = mongoose.model("Exam", examSchema);
 
 // Create exam
